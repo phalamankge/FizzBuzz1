7d4128ff9d9b709da4ba77132af255a44ee9b70c
phalamankge <74191830+phalamankge@users.noreply.github.com>
2022-02-21 09:03:48 +0200

    Add files via upload

---
+++ FizzBuzz1/FizzBuzzer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz1
+{
+    public FizzBuzzer()
+    {
+
+    }
+    public bool  Print1To100()
+    {
+         bool ret = false;
+        
+        for(int i = 1; 1 <= 100; i++)
+        {
+            Console.WriteLine(i + ",");
+        }
+
+        ret = true;
+        return ret;
+    }
+
+    public bool MultiplesOfThreee()
+    {
+        bool ret = false;
+
+        for (int i = 1; 1 <= 100; i++)
+        {
+            if (i % 3 == 0)
+            {
+                Console.WriteLine(i + ","+"Fizz" + " ");
+            }
+            else if (i % 5 == 0)
+            {
+                Console.WriteLine(i + "," + "Buzz" + " ");
+            }
+            else if (i % 3 == 0 && i % 5 == 0)
+            {
+                Console.WriteLine(i + "," + "Buzz" + " ");
+            }
+            else
+            {
+                Console.WriteLine(i + ",");
+            }   
+            
+        }
+
+        ret = true;
+        return ret;
+    }
+}
---
+++ FizzBuzz1/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+
+
+namespace FizzBuzz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzzer fizzBuzzer = new FizzBuzzer();
+            FizzBuzzer.Print1To100();
+
+            
+        }
+    }
+}
